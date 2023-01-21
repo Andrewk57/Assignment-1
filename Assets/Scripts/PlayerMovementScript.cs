5647fea5c0cdfd746fd4d28b537d1444ba96f106
Andrewk57 <112024432+Andrewk57@users.noreply.github.com>
2023-01-21 15:48:15 -0500

    movement updated added jump

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float MovementSpeed;
+    public float RotationSpeed;
+    public float JumpHeight;
+    public bool IsForward = false;
+    
+    public Rigidbody2D rb;
+  
+    private float move;
+    public bool isJump;
+    
+
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Jump()
+
+    {
+       // if (Time.time - lastJump < JumpCoolDown)
+       // {
+      //      return;
+       // }
+        //lastJump = Time.time;
+        transform.position += transform.up * JumpHeight * Time.deltaTime;
+    }
+    void MoveForward()
+    {
+        Debug.Log("Triggered movement");
+        if (Time.timeScale >= 1f)
+        {
+            move = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(MovementSpeed * move, rb.velocity.y);
+            IsForward = true;
+        }
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        MoveForward();
+        float rotation = 0f; // variable to store rotation amount
+
+        if (Input.GetKey(KeyCode.A) && Time.timeScale >= 1f)
+        {
+            rotation = RotationSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D) && Time.timeScale >= 1f)
+        {
+            rotation = -RotationSpeed;
+        }
+        float rotationAmount = rotation * Time.deltaTime;
+        transform.Rotate(0f, 0f, rotationAmount);
+
+        if (IsForward == true)
+        {
+            Debug.Log("Smoke");
+        }
+        if (Input.GetButtonDown("Jump") && isJump == false && Time.timeScale >= 1f)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, JumpHeight));
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Floor"))
+        {
+            isJump = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isJump = true;
+        }
     }
 }
